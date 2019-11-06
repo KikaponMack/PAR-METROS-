3de8be09c749e3e1232908db396a1e3783b37e63
LUCHI <Alumno@PC34>
2019-11-06 11:28:53 -0600

    aasiesss

---
+++ PARAMETROS/PARAMETROS/PARAMETROS.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <iostream>
+using namespace std;
+//Declaramos prototipo de funcion
+int invertir(int num);
+int main()
+{
+	int num, resultado;
+	cout << "Introude un numero entero: ";
+	cin >> num;
+	resultado = invertir(num);
+	cout << "Numero original: " << num << endl;
+	cout << "Numero invertido: " << resultado << endl;
+	system("pause");
+	return 0;
+}
+//Declaramos la funcion
+int invertir(int num)
+{
+	int inverso = 0, cifra;
+	while (num != 0)
+	{
+		cifra = num % 10;
+		inverso = inverso * 10 + cifra;
+		num = num / 10;
+	}
+	return inverso;
+}
